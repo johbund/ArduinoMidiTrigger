16b099d8655aee21aa59f041e4fdc8b1e6feaf33
Johannes Bund <jbund@mpi-inf.mpg.de>
2021-02-14 14:09:40 +0100

    Code makeover for the testsketch

--- ArduinoTriggerTest/ArduinoTriggerTest.ino
+++ ArduinoTriggerTest/ArduinoTriggerTest.ino
@@ -1,97 +1,174 @@
-#define SENSOR_PIN A3
+// ArduinoTriggerTest; Test a piezo drum pad.
+// Copyright (C) 2021  Johannes Bund
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#define THD 25
-#define MAX_VALUE 800
 
-#define AVG 5
+// ##################################################### preliminary definitions
+#define MIDI_NOTE_ON    0x90
+#define MIDI_NOTE_OFF   0x80
 
-#define COOLDOWN 50
+#define MIDI_BAUD       31250
+#define SERIAL_BAUD     9600
 
-int sensVal_avg;
-int sensVal_old;
-int sensVal_old_old;
+// Midi channel (Arturia DrumBrute)
+#define MIDI_CHANNEL    10
 
-unsigned long lasttime;
+// Arturia DrumBrute Midi map
+#define DB_KICK1        0x24
+#define DB_KICK2        0x25
+#define DB_SNARE        0x26
+#define DB_CLAP         0x27
+#define DB_RIM          0x28
+#define DB_CLAVES       0x29
+#define DB_CLHAT        0x2a
+#define DB_OPHAT        0x2b
+#define DB_CONGAH       0x2c
+#define DB_TOMH         0x2d
+#define DB_CONGAL       0x2e
+#define DB_TOML         0x2f
+#define DB_CYMBAL       0x30
+#define DB_REVCYMBAL    0x31
+#define DB_MARACAS      0x32
+#define DB_TAMB         0x33
+#define DB_ZAP          0x34
 
-int i;
 
-float sensVal[AVG];
+// ############################################# adjust the following parameters
+#define SENSOR_PIN A0
 
-bool note_ready;
+#define AVERAGE 5           // number of last measurements to average over
 
+#define THRESHOLD 25        // threshold for sensor readings
+
+#define MAX_VALUE 800       // clip sensor values at MAX_VALUE
+
+#define COOLDOWN 50         // time until next note can be fired (in millis)
+
+
+int sensorValues[AVERAGE];  // array of last sensor measurements
+unsigned int writePos;      // write position of next sensor measurement
+
+float currValue;            // current average over sensor measuruments 
+float prevValue;            // previous average over sensor measuruments 
+
+bool noteReady;             // indicates that next note can be triggered
+
+unsigned long lasttime;     // save time of last note trigger
+
+
+// ####################################################################### setup
 void setup() {
-  Serial.begin(31250);
+  int i;
 
-  sensVal_avg = 0;
-  sensVal_old = 0;
-  sensVal_old_old = 0;
-
-  lasttime = 0;
-
-  for (i = 0; i < AVG; i++) {
-    sensVal[i] = 0;
+  // initialize variables
+  for (i = 0; i < AVERAGE; i++) {
+    sensorValues[i] = 0;
   }
+  writePos = 0;
+  currValue = 0.0;
+  prevValue = 0.0;
+  lasttime = 0;
+  noteReady = false;
   
-  note_ready = false;
+  Serial.begin(SERIAL_BAUD);
 }
 
+
+// ######################################################################## loop
 void loop() {
+  int i;
+  char output[32];
 
-  for (i = 1; i < AVG; i++) {
-    sensVal[i] = sensVal[i-1];
-  }
-  sensVal[0] = analogRead(SENSOR_PIN);
+  writePos = (writePos + 1) % AVERAGE;              // advance write position
+  sensorValues[writePos] = analogRead(SENSOR_PIN);  // read sensor
 
-  double sum = 0;
-  for (i = 0; i < AVG; i++) {
-    sum += sensVal[i];
+  unsigned int sum = 0;                             // compute average
+  for (i = 0; i < AVERAGE; i++) {
+    sum += sensorValues[i];
   }
-  sensVal_avg = (int) (sum / (float) AVG);
+  currValue = sum / (float) AVERAGE;
   
-  if (sensVal_avg > THD) {
-
-//  Serial.print(read_sens);
-//  Serial.print(", ");
-  Serial.print(sensVal_avg);
-//  Serial.print(", ");
-//  Serial.print((int) (((ppSensVal + pSensVal + sensVal) / 3.0));
-  Serial.println();
+  if (currValue > THRESHOLD) {
     
-//    Serial.println(sensorVal);
-    if (sensVal_avg >= sensVal_old) {
-      note_ready = true;
-    } 
-
-    if (sensVal_avg < sensVal_old && sensVal_old < sensVal_old_old) {
-      if (note_ready) {
+    sprintf(output, "  sensor: %d, average: %d.%02d", sensorValues[writePos], 
+                             (int)currValue, (unsigned int)(currValue*100)%100);
+    Serial.println(output);
+    
+    if (currValue >= prevValue) {
+      noteReady = true;
+    } else {
+      if (noteReady) {
         if (lasttime + COOLDOWN < millis()) {
         
-        noteOn(0x99, 0x27, velocityMap(sensVal_old_old)); //note_on
-        noteOn(0x89, 0x27, velocityMap(sensVal_old)); //note_off
+        noteOn(DB_CLAP, velocityMap(prevValue));  //note_on
+        noteOff(DB_CLAP);                         //note_off
 
         lasttime = millis();
-        note_ready = false;
+        noteReady = false;
         }
       }
     }
 
-  sensVal_old_old = sensVal_old;
-  sensVal_old = sensVal_avg;
+  prevValue = currValue;
   }
 }
 
-int velocityMap(int value) {
-  if (value > MAX_VALUE) {
-    return 127;
+
+// ####################################################### function declarations
+
+// map sensor value to range 0 to MAX_VALUE
+int velocityMap(float value) {
+  char output[32];
+  int result;
+
+  if (value >= MAX_VALUE) {
+    result = 127;
   }
-  float percent = (float) value / (float) MAX_VALUE;
-//  Serial.println(percent);
-  return (int) (percent * 127); 
+  float percent = value / (float) MAX_VALUE;
+  result = (int) (percent * 127 + 0.5);
+
+  sprintf(output, "  velocity map %d.%02d to %d", (int)value, 
+                            (unsigned int)(value*100)%100, result);
+  Serial.println(output);
+
+  return result;
 }
 
-void noteOn(int cmd, int pitch, int velocity) {
+void noteOn(int pitch, int velocity) {
+  char output[32];
+  sprintf(output, "MIDI note %x on, velocity: %d", pitch, velocity);
+  Serial.println(output);
+
+/*  // change baud rate when sending Midi signals
+  int cmd = MIDI_NOTE_ON + MIDI_CHANNEL - 1;
   Serial.write(cmd);
   Serial.write(pitch);
   Serial.write(velocity);
-//  Serial.print("Midi");
+*/
+}
+
+void noteOff(int pitch) {
+  char output[32];
+  sprintf(output, "MIDI note %x off", pitch);
+  Serial.println(output);
+  
+/*  // change baud rate when sending Midi signals
+  int cmd = MIDI_NOTE_OFF + MIDI_CHANNEL - 1;
+  int velocity = 0x00;
+  Serial.write(cmd);
+  Serial.write(pitch);
+  Serial.write(velocity);
+*/
 }
